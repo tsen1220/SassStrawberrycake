6b7cb7be75eb5146813443b4950cdf4680ef2af6
TzuEn Tsen <tsen1220.us@gmail.com>
2019-08-07 02:40:03 +0800

    finish

--- candlekill.js
+++ candlekill.js
@@ -6,12 +6,39 @@ var test=anime.timeline({
     autoplay:false,
 });
 
+
 test.add({
     targets:"#test",
     opacity:1,
-    duration:3000,
-    delay:500,
+    duration:2000,
     top:'100px',
 })
+
+test.add({
+    targets:".bal",
+    opacity:{
+        value:'1',
+    duration:500},
+    duration:10000,
+    delay:500,
+    translateY:'-500px',
+    translateX:function(){
+        return anime.random(-40,40)
+    },
+    rotate:function(){
+        return anime.random(-60,60)
+    }
+
+    }
+)
+
+test.add({
+    targets:".bal",
+    opacity:0,
+    duration:3000,
+    delay:500,
+  
+})
+
 
 document.querySelector(".flame").addEventListener('click',()=>{test.play()})
